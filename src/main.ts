77baf79c3eab29bfb8a25dc7baef837b6301368c
AndreyArthur <twekylaywer@gmail.com>
2021-10-18 17:54:25 -0300

    fix: import module after alias setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import path from 'path';
 import { addAlias } from 'module-alias';
 
-import { server } from '@/infra/http';
-
 addAlias('@', path.resolve(__dirname));
+
+// eslint-disable-next-line
+import { server } from '@/infra/http';
 
 server.start();
